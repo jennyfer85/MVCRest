20e4e6ddd830627245b670cdb8c3ece30b2a3c3b
jennyfer85 <jmtg05@gmail.com>
2024-10-01 01:24:15 +0200

    running Node code

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hola Mundo');
+//import {}
+
+//const express = require('express'); //Versión antigua
+
+//crear la app
+const app = express(); //instacia de express
+
+//Routing - Endpoint que soporta nuestra aplicación
+app.get('/', function(req, res) {
+    res.json({msg:'Hola mundo en express'}) //devuelve los datos tipo json
+})
+
+app.get('/nosotros', function(req, res) {
+    res.send('Información de nosotros') //muestra la información
+})
+
+
+//Definir un puerto y arrancar el proyecto
+const port = 3000
+app.listen(port, () =>{
+    console.log('El servidor esta funcionando en el puerto ${port}')
+});
